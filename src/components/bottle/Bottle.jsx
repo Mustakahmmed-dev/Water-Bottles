1f813ff236cf2290a17d29767535d7b6ee1a3e00
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-03-30 15:49:24 +0600

    Contents updated

--- src/components/bottle/Bottle.jsx
+++ src/components/bottle/Bottle.jsx
@@ -1,20 +1,20 @@
 import './Bottle.css';
 
-export default function Bottle({bottle, handleAddToCartBtn}){
+export default function Bottle({ bottle, handleAddToCartBtn }) {
     // console.log(bottle)
 
-   
+
 
     return (
 
-         <div className="bottle-card">
+        <div className="bottle-card">
             <img className='bottle-img' src={bottle.img} alt="Water bottle" />
             <div>
                 <p>Name: {bottle.name} </p>
-                <p>Price: {bottle.price} </p>
+                <p>Price: {bottle.price}$ </p>
                 <p>Stock: {bottle.stock} </p>
             </div>
             <button onClick={() => handleAddToCartBtn(bottle)}>Add To Cart</button>
-       </div>
+        </div>
     )
 }
